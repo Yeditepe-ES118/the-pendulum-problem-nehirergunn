23a90c904facfa70083498233b5b974025c34e46
nehirrgunn <nehir.ergun@std.yeditepe.edu.tr>
2025-11-01 02:53:45 +0300

    Add assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -4,16 +4,27 @@ Created on Sat Nov  1 01:34:56 2025
 
 @author: aergu
 """
+import numpy as np
 
-import numpy as np 
 def find_period(L0, L1):
-    g=9.81 #gravity in m/s**2
-    T0= 2*np.pi*np.sqrt(L0/g)  # period for L0
-    T1 = 2*np.pi*np.sqrt(L1/g)  # period for L1
+    g = 9.81  # gravity (m/s^2)
 
-    for L in range(L0, L1+1):
-        T = 2*np.pi*np.sqrt(L/g)
-        print("When L = %4.1f m, T = %4.1f s" % (L, T))
-    
+    T0 = 2 * np.pi * np.sqrt(L0 / g)  # period at L0 (s)
+    T1 = 2 * np.pi * np.sqrt(L1 / g)  # period at L1 (s)
+
+    for L in range(L0, L1 + 1):
+        T = 2 * np.pi * np.sqrt(L / g)  # period at L (s)
+        _ = T  # keep loop active
+
     return T0, T1
 
+
+if __name__ == "__main__":
+    L0, L1 = 2, 10
+    g = 9.81
+
+    for L in range(L0, L1 + 1):
+        T = 2 * np.pi * np.sqrt(L / g)
+        print("When L = %4.1f m, T = %4.1f s" % (L, T))
+
+    T0, T1 = find_period(L0, L1)
